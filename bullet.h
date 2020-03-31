5db626bfbf8bfdd1630a25023868539ec5df1807
quinnvoker <gareth.branscombe@gmail.com>
2020-03-31 15:03:05 -0700

    Implemented Bullet, removed summator example

---
+++ bullet.h
@@ -0,0 +1,47 @@
+#ifndef BULLET_H
+#define BULLET_H
+
+#include "scene/2d/node_2d.h"
+#include "bullet_type.h"
+
+class Bullet : public Node2D
+{
+    GDCLASS(Bullet, Node2D);
+
+    private:
+        bool active;
+        float lifetime;
+
+        Vector2 direction;
+        Vector2 perp_offset;
+
+        Ref<BulletType> type;
+    
+    protected:
+        void _notification(int p_what);
+
+        static void _bind_methods();
+
+    public:
+        Bullet();
+        ~Bullet();
+
+        void _ready();
+
+        void move(float delta);
+
+        void set_active(bool value);
+        bool get_active();
+
+        void set_lifetime(float value);
+        float get_lifetime();
+
+        void set_direction(Vector2 value);
+        Vector2 get_direction();
+
+        void set_type(Ref<BulletType> value);
+        Ref<BulletType> get_type();
+};
+
+
+#endif
--- register_types.cpp
+++ register_types.cpp
@@ -3,12 +3,12 @@
 #include "register_types.h"
 
 #include "core/class_db.h"
-#include "summator.h"
 #include "bullet_type.h"
+#include "bullet.h"
 
 void register_qurobullet_types() {
-    ClassDB::register_class<Summator>();
     ClassDB::register_class<BulletType>();
+    ClassDB::register_class<Bullet>();
 }
 
 void unregister_qurobullet_types() {
