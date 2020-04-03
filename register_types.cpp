1a0f8df5eb9cf46130e4170e40b58bfe738d937b
quinnvoker <gareth.branscombe@gmail.com>
2020-04-03 14:56:17 -0700

    Added BulletServerRelay Singleton

--- register_types.cpp
+++ register_types.cpp
@@ -3,16 +3,21 @@
 #include "register_types.h"
 
 #include "core/class_db.h"
+#include "core/engine.h"
 #include "bullet_type.h"
 #include "bullet.h"
 #include "bullet_server.h"
+#include "bullet_server_relay.h"
 
 void register_qurobullet_types() {
     ClassDB::register_class<BulletType>();
     ClassDB::register_class<Bullet>();
     ClassDB::register_class<BulletServer>();
+    ClassDB::register_class<BulletServerRelay>();
+
+    Engine::get_singleton()->add_singleton(Engine::Singleton("BulletServerRelay", memnew(BulletServerRelay)));
 }
 
-void unregister_qurobullet_types() {
-   // Nothing to do here in this example.
+void unregister_qurobullet_types() 
+{
 }
