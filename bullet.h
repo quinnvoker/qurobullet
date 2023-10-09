154528b11be376365b5bf15e60de04612ed4588b
Quinn <quinnvoker@gmail.com>
2023-10-08 20:01:00 -0700

    Fix include errors

--- bullet.h
+++ bullet.h
@@ -1,13 +1,12 @@
 #ifndef BULLET_H
 #define BULLET_H
 
-#include "core/object/object.h"
-#include "core/object/reference.h"
 #include "bullet_type.h"
-#include "core/math/transform_2d.h"
 #include "core/math/rect2.h"
+#include "core/math/transform_2d.h"
+#include "core/object/object.h"
+#include "core/object/ref_counted.h"
 #include "scene/resources/texture.h"
-
 
 class Bullet : public Object {
 	GDCLASS(Bullet, Object);
@@ -27,41 +26,41 @@ class Bullet : public Object {
 	void _update_offset();
 	void _update_appearance(const Ref<BulletType> &p_type = NULL);
 
-	protected:
-		static void _bind_methods();
+protected:
+	static void _bind_methods();
 
-	public:
-		void spawn(const Ref<BulletType> &p_type, const Vector2 &p_position, const Vector2 &p_direction);
+public:
+	void spawn(const Ref<BulletType> &p_type, const Vector2 &p_position, const Vector2 &p_direction);
 
-		void update(float delta);
+	void update(float delta);
 
-		void pop();
-		bool is_popped();
+	void pop();
+	bool is_popped();
 
-		bool can_collide();
+	bool can_collide();
 
-		void set_time(float p_time);
-		float get_time() const;
+	void set_time(float p_time);
+	float get_time() const;
 
-		void set_type(const Ref<BulletType> &p_type);
-		Ref<BulletType> get_type() const;
+	void set_type(const Ref<BulletType> &p_type);
+	Ref<BulletType> get_type() const;
 
-		void set_direction(const Vector2 &p_direction);
-		Vector2 get_direction() const;
+	void set_direction(const Vector2 &p_direction);
+	Vector2 get_direction() const;
 
-		void set_position(const Vector2 &p_position);
-		Vector2 get_position() const;
+	void set_position(const Vector2 &p_position);
+	Vector2 get_position() const;
 
-		void set_rotation(float p_radians);
-		float get_rotation() const;
+	void set_rotation(float p_radians);
+	float get_rotation() const;
 
-		Transform2D get_transform();
+	Transform2D get_transform();
 
-		void set_ci_rid(const RID &p_rid);
-		RID get_ci_rid() const;
+	void set_ci_rid(const RID &p_rid);
+	RID get_ci_rid() const;
 
-		Bullet();
-		~Bullet();
+	Bullet();
+	~Bullet();
 };
 
 #endif
--- register_types.cpp
+++ register_types.cpp
@@ -7,8 +7,8 @@
 #include "bullet_server_relay.h"
 #include "bullet_spawner.h"
 #include "bullet_type.h"
-#include "core/class_db.h"
-#include "core/engine.h"
+#include "core/config/engine.h"
+#include "core/object/class_db.h"
 
 BulletServerRelay *_bullet_server_relay = nullptr;
 
